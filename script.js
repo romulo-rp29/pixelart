4b0286e6f0065aceef99a67d45254a4d0037ff5b
romulo-rp29 <romul.ceuma@gmail.com>
2022-01-02 21:09:18 -0300

    voltei para o passo 5

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
 let selected = document.getElementsByClassName('selected')[0]
 
-let color = window.getComputedStyle( selected).getPropertyValue('background-color');
-console.log(color)
+let paint = window.getComputedStyle(selected).getPropertyValue('background-color');
+console.log(paint)
 
 let pixels = document.getElementById('pixel-board').children
 
-for(let pixel of pixels){
+for (let pixel of pixels) {
    
     pixel.addEventListener('click',function(event){
-        event.target.style.backgroundColor = color;
+        event.target.style.backgroundColor = paint;
     });
 
 }
